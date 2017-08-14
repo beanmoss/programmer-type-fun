4855337ab712a2c073b4aebcc506f9412842eee0
Robel <norsu.robel@gmail.com>
2017-08-14 21:38:15 +0800

    added root url

--- templates/main.php
+++ templates/main.php
@@ -1,20 +1,20 @@
 <html>
 <head>
+  <?php $root_url = 'http://rrluna.com/programmer-type-fun'?>
   <title><?=$this->e($title)?></title>
   <meta property="og:url"         content="<?=$this->e($url)?>" />
   <meta property="fb:app_id"         content="312923542405324" />
   <!-- <meta property="og:type"        content="article" /> -->
   <meta property="og:title"       content="<?=$this->e($title)?>" />
   <meta property="og:description" content="<?=$this->e($description)?>" />
-  <meta property="og:image"       content="<?=$this->e($image)?>" />
-  <meta property="og:image"       content="<?=$this->e($image)?>" />
+  <meta property="og:image"       content="<?=$root_url . $this->e($image)?>" />
   <meta property="og:image:width"       content="600" />
   <meta property="og:image:height"       content="315" />
 
 </head>
 <body>
 <h3><?=$this->e($title)?></h3>
-<img src="<?=$this->e($image)?>" title="<?=$this->e($title)?>" />
+<img src="<?=$root_url . $this->e($image)?>" title="<?=$this->e($title)?>" />
 <p>Source: <a href="https://www.techinasia.com/11-types-programmers-infographic">https://www.techinasia.com/</a> </p>
 </body>
 </html>
