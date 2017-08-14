c96323f0d833053d0e7b144f0d3d35c06d7b55bd
Robel <norsu.robel@gmail.com>
2017-08-14 21:54:42 +0800

    added url

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require 'vendor/autoload.php';
+
 // Create new Plates instance
 $templates = new League\Plates\Engine('templates');
 
@@ -8,5 +9,16 @@ $programmer_types_string = file_get_contents('programmer_types.json');
 $programmer_types = json_decode($programmer_types_string, true);
 
 $rand_keys = array_rand($programmer_types);
+$chosen = $programmer_types[$rand_keys];
+$chosen['type'] = $rand_keys;
+
+if (!isset($_GET['type'])) {
+    $chosen = $chosen;
+} elseif (isset($programmer_types[$_GET['type']])) {
+    $chosen = $programmer_types[$_GET['type']];
+    $chosen['type'] = $_GET['type'];
+}
+
+var_dump($chosen);
 // Render a template
-echo $templates->render('main', $programmer_types[$rand_keys]);
+echo $templates->render('main', $chosen);
